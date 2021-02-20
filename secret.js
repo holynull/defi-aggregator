193c8e6d4f49b28f9669334114c4485ed7f5d095
Holy Null <holynull@126.com>
2021-02-20 15:10:41 +0800

    Delete secret.js

--- secret.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    mnemonic: "song saddle glide prosper clean film fall tank execute toward sauce item",
-    // mnemonic: "style narrow voyage brief grab impose kiwi fresh route search garage strategy",
-    // projectId: "4d1446a24a744518a10686badaa1119d"
-    projectId: "a1b8fe06fc1349b1b812bdb7b8f79465"
-
-};
